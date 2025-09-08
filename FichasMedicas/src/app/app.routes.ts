d066727850065755b0b6e9cf919019ff02f01feb
IgnaciaCG <168758049+IgnaciaCG@users.noreply.github.com>
2025-09-08 17:39:39 -0300

    yes

--- FichasMedicas/src/app/app.routes.ts
+++
@@ -1,5 +0,0 @@
-import { Routes } from '@angular/router';
-export const routes: Routes = [
-  { path:'', redirectTo:'principal', pathMatch:'full' },
-  { path:'principal', loadComponent:()=>import('./pages/principal/principal.page').then(m=>m.PrincipalPage) },
-];
